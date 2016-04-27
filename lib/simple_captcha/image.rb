29ed0f7cf12bdbe65888b657d172460a1db38652
Yuri <yuri@simplee.com>
2016-04-27 18:41:45 +0300

    add generate_simplee_captcha_image method

--- lib/simple_captcha/image.rb
+++ lib/simple_captcha/image.rb
@@ -67,7 +67,7 @@ module SimpleCaptcha #:nodoc
       params << "-pointsize #{SimpleCaptcha.point_size}"
       params << "-implode 0.2"
 
-      dst = Tempfile.new(RUBY_VERSION < '1.9' ? 'simple_captcha.jpg' : ['simple_captcha', '.jpg'], SimpleCaptcha.tmp_path)
+      dst = Tempfile.new(RUBY_VERSION < '1.9' ? 'simple_captcha.jpg' : ['simple_captcha', '.jpg'], ::SimpleCaptcha.tmp_path)
       dst.binmode
 
       #params << "label:#{text} '#{File.expand_path(dst.path)}'"
@@ -80,5 +80,32 @@ module SimpleCaptcha #:nodoc
       File.expand_path(dst.path)
       #dst
     end
+
+    def generate_simplee_captcha_image(simple_captcha_key, simple_captcha_value) #:nodoc
+      amplitude, frequency = ImageHelpers.distortion(SimpleCaptcha.distortion)
+      text = simple_captcha_value
+
+      params = ImageHelpers.image_params(SimpleCaptcha.image_style).dup
+      params << "-size #{SimpleCaptcha.image_size}"
+      params << "-wave #{amplitude}x#{frequency}"
+      #params << "-gravity 'Center'"
+      params << "-gravity \"Center\""
+      params << "-pointsize #{SimpleCaptcha.point_size}"
+      params << "-implode 0.2"
+
+      dst = Tempfile.new(RUBY_VERSION < '1.9' ? 'simple_captcha.jpg' : ['simple_captcha', '.jpg'], ::SimpleCaptcha.tmp_path)
+      dst.binmode
+
+      #params << "label:#{text} '#{File.expand_path(dst.path)}'"
+      params << "label:#{text} \"#{File.expand_path(dst.path)}\""
+
+      SimpleCaptcha::Utils::run("convert", params.join(' '))
+
+      dst.close
+
+      File.expand_path(dst.path)
+      #dst
+    end
+
   end
 end
